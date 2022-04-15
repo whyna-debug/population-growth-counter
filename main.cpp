c98e94d19d8b69697eda4eb7febb2f361c534f76
Wahyuna <89669786+Wahyunaa@users.noreply.github.com>
2022-04-15 07:51:26 +0700

    Add files via upload
    first

---
+++ main.cpp
@@ -0,0 +1,105 @@
+// a population growth counter
+//
+// You can use this file for anything, 
+// and you can also develop this software according to what you want.
+//
+// Give me a star on https://github.com/Wahyunaa/population-growth-counter
+//
+// Copyright (C) 2022 Wahyuna.
+
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+using namespace std;
+int main(){system("cls");
+	unsigned int maxLine = 35, language;
+	string middle = " ";
+	short menu;
+	for(int LanguageMenu = 1; LanguageMenu <2;){
+  for(int line = 1; line <= maxLine; line++ ){
+  	cout << "_";
+  }
+  cout << endl << endl;
+	#include "info/software.cpp"
+	for(int i=1;i<=5;i++){
+  cout << middle;
+}
+  cout << softwareName << endl;
+  for(int line = 1; line <= maxLine; line++ ){
+  	cout << "_" ;
+  }
+  cout << endl << endl;
+  cout << "Language: \n\n [1] English \n [2] indonesia\n\n";
+  cout << "select a language: ";
+  cin >> language;
+  if(language == 1){
+    for (short ifTrue = 1; ifTrue < 2;){
+    system("cls");
+for(int line = 1; line <= maxLine; line++ ){
+  	cout << "_";
+  }
+  cout << endl << endl;
+ for(int i=1;i<=5;i++){
+  cout << middle;
+}
+  cout << softwareName << endl;
+  for(int line = 1; line <= maxLine; line++ ){
+  	cout << "_" ;
+  }
+  cout << endl << endl; 
+    // default language
+    LanguageMenu = 2;
+    cout << "Menu:\n\n [1] Total\n [2] Natural"<< endl;
+  cout << "\nSelect menu: ";
+  cin >> menu;
+  if(menu == 1){system("cls");
+  ifTrue = 2;
+for(int line = 1; line <= maxLine; line++ ){
+  	cout << "_";
+  }
+  cout << endl << endl;
+ for(int i=1;i<=5;i++){
+  cout << middle;
+}
+  cout << softwareName << endl;
+  for(int line = 1; line <= maxLine; line++ ){
+  	cout << "_" ;
+  }
+  cout << endl << endl;
+  long lm, ie, l, m, i, e, pt; cout << "Total population growth counter\n\n Birth: "; cin >> l;  cout << " Dead: ";  cin >> m;  cout << " Immigration: ";  cin >> i;  cout << " Emigration: ";  cin >> e;  lm = l - m;  ie = i - e;  pt = lm + ie;  cout << endl << "Total population growth rate: " << pt << endl;
+  } else if(menu == 2){system("cls");
+  ifTrue = 2;
+for(int line = 1; line <= maxLine; line++ ){
+  	cout << "_";
+  }
+  cout << endl << endl;
+ for(int i=1;i<=5;i++){
+  cout << middle;
+}
+  cout << softwareName << endl;
+  for(int line = 1; line <= maxLine; line++ ){
+  	cout << "_" ;
+  }
+  cout << endl << endl;                 
+     long pa, l, m;
+     cout << "Natural population growth counter\n\n Birth: ";
+     cin >> l; cout << " Dead: "; cin >> m; pa = l - m; cout << endl;  cout << "Natural population growth rate: " << pa << endl;
+  }else{
+    cout << "\n\nPlease choose correctly! \n" << endl;
+    sleep(1);
+     system("cls");
+  }
+    }
+  } else if (language == 2){
+   // indonesia language
+   LanguageMenu = 2;
+   #include "language/id.cpp"
+  } else {
+    cout << "\nPlease choose correctly!\n\n";
+  sleep(1);
+    system("cls");
+  }
+    }
+  cin.get();
+  return 0;
+}
---
+++ readme.md
@@ -0,0 +1,26 @@
+##Population growth counter
+<center>
+  <br />
+ <img src="assets/img/logo.jpg" alt="logo" width="340"/> <br />
+ <br />
+ </center>
+ <section id="About">
+###About
+<p>This is a software to calculate population growth.</p>
+<ul>
+  <li>Natural</li>
+  <li>Total</li>
+</ul>
+</section>
+<br />
+###Available languages
+ <ul>
+   <li>English</li>
+   <li>Indonesia</li>
+ </ul>
+<br />
+<section id="open-source">
+###Open source
+<p>you can use this software for anything, <br />and you can also develop this software according to what you want.</p>
+</section># population-growth-counter
+# population-growth-counter
