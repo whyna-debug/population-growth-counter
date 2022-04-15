0196770bc1b533106d7c833c1eee80c54587dfa9
Wahyuna <89669786+Wahyunaa@users.noreply.github.com>
2022-04-15 08:04:15 +0700

    Create software.cpp

---
+++ info/software.cpp
@@ -0,0 +1,10 @@
+// software info
+//
+// You can use this file for anything,
+// and you can also develop this software according to what you want.
+//
+// Give me a star on https://github.com/Wahyunaa/population-growth-counter
+//
+// Copyright (C) 2022 Wahyuna.
+
+string softwareName = "population growth counter";
