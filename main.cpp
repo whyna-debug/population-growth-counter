0f51e5563b40497d8fdca3119deb494bb99ae411
Wahyuna <89669786+Wahyunaa@users.noreply.github.com>
2022-04-23 08:30:58 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // a population growth counter
 //
-// You can use this file for anything, 
-// and you can also develop this software according to what you want.
+// This software is free to use,
+// and you can also develop this software as you wish.
 //
 // Give me a star on https://github.com/Wahyunaa/population-growth-counter
 //
